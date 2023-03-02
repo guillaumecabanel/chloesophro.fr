b364f7ec65406f44f9a39762f550dc5525dbf1a0
Guillaume CABANEL <guillaume@kuartz.fr>
2023-03-02 14:02:23 +0100

    add some content

--- config.rb
+++ config.rb
@@ -1,15 +1,21 @@
-activate :external_pipeline,
-  name: :tailwind,
-  command: "npx tailwindcss -i ./source/stylesheets/site.css -o ./dist/stylesheets/site.css #{ build? ? '--minify' : '--watch'}",
-  latency: 2,
-  source: "./dist/"
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+set :css_dir, "assets/stylesheets"
+set :fonts_dir, "assets/fonts"
+set :images_dir, "assets/images"
+set :js_dir, "assets/javascripts"
+
+activate :external_pipeline,
+  name: :tailwind,
+  command: "npx tailwindcss -i ./source/assets/stylesheets/site.css -o ./dist/assets/stylesheets/site.css #{ build? ? '--minify' : '--watch'}",
+  latency: 2,
+  source: "./dist/"
+
 configure :build do
   activate :gzip
   activate :asset_hash
+  activate :minify_html
   activate :minify_javascript
 end
